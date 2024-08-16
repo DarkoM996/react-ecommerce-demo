0f5238934920bed42d96360aebcbf0f84e026e82
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-08-16 19:36:05 +0200

    latest

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full h-full text-4xl font-opensans border-b border-neutral-300">
+    <div className="max-w-screen-3xl mx-auto w-full h-20 text-4xl font-opensans border-b border-neutral-300">
       Navbar
     </div>
   );
