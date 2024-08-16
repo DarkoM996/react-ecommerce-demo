e2ea409a6f9c941bf61fa494fd1acb9440e33137
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-08-16 19:49:15 +0200

    Additional set up for starters

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { MdOutlineShoppingBag } from "react-icons/md";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen-3xl mx-auto w-full h-20 text-4xl font-opensans border-b border-neutral-300">
-      Navbar
+    <div className="max-w-screen-3xl mx-auto w-full h-20  border-b border-neutral-300">
+      <div className="flex justify-between items-center w-full h-full px-6">
+        <a href="/" className="text-3xl font-semibold text-red-400">
+          TypeLogo
+        </a>
+        {/* another div */}
+        <ul className="flex justify-center items-center gap-6">
+          <li>Home</li>
+          <li>Products</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        {/* Shopping Icon */}
+        <MdOutlineShoppingBag className="text-2xl" />
+      </div>
     </div>
   );
 };
