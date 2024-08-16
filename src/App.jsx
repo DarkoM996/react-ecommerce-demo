a5cee7e888fdb6f08308b7e27ed2903abb1ba61d
DarkoM996 <darko.mihajlovski96@gmail.com>
2024-08-16 19:01:52 +0200

    Initial start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>Some text for starting</p>
+    </>
+  );
+}
+
+export default App;
